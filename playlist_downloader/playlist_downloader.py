45688f167e81475d7b3ab3dd05155933979e3c05
Legion <abhaykabdwal@gmail.com>
2023-05-03 17:02:42 +0530

    finished playlist downloader

--- playlist_downloader/playlist_downloader.py
+++ playlist_downloader/playlist_downloader.py
@@ -8,36 +8,29 @@ folder = ".\YT"
 videos=list(Playlist(playlist_link))
 
 for i in range(len(videos)):
-    for _ in range(3):
-        try:
-            yt   = YouTube(videos[i], use_oauth=True, allow_oauth_cache=True)
-            yd = yt.streams.get_highest_resolution()
-            video_title = yt.title   # title of video
-            video_views = yt.views   # views in video
-            video_length = yt.length # length of video in seconds
-            video_size = yd.filesize # size of video in bytes
+    try:
+        yt   = YouTube(videos[i], use_oauth=True, allow_oauth_cache=True)
+        yd = yt.streams.get_highest_resolution()
+        video_title = yt.title   # title of video
+        video_views = yt.views   # views in video
+        video_length = yt.length # length of video in seconds
+        video_size = yd.filesize # size of video in bytes
 
-            print("\nTitle: ", video_title)
-            print("Views: ", video_views)
-            print("Length: ", video_length//60, "minutes", video_length%60, "seconds")
-            print("Size: ", video_size/(2**20), "MB")
+        print("\nTitle: ", video_title)
+        print("Views: ", video_views)
+        print("Length: ", video_length//60, "minutes", video_length%60, "seconds")
+        print("Size: ", video_size/(2**20), "MB")
 
-            time.sleep(1)
-            print("Donwloading... ")
+        time.sleep(1)
+        print("Donwloading... ")
 
-            # Add folder path here
-            yd.download(folder)
+        # Add folder path here
+        yd.download(folder)
 
-            print("Download complete!")
-            print("\n------------------------------------")
-            videos.remove(videos[i])
-            break
-    
-        except Exception:
-            print(f"\nAn error occurred")
-            continue
+        print("Download complete!")
+        print("\n------------------------------------")
 
-with open("links.txt","a") as myfile:
-    for video in videos:
-        myfile.write(video)
-print("Done")
+    except Exception:
+        print(f"\nAn error occurred")
+
+print("\n ------------------Done------------------------\n")
