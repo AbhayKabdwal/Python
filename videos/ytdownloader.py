9b46bad0e34fbacef6d604f6c6f2975767b8a678
Legion <abhaykabdwal@gmail.com>
2023-05-03 17:13:52 +0530

    completed ytdownloader

--- videos/ytdownloader.py
+++ videos/ytdownloader.py
@@ -9,35 +9,35 @@ with open(filename) as linksfile:
 
 
 for link in links:
-    for _ in range(10):
-        # try:
-        yt = YouTube(link)
-        yd = yt.streams.get_highest_resolution()
-        video_title = yt.title   # title of video
-        video_views = yt.views   # views in video
-        video_length = yt.length # length of video in seconds
-        video_size = yd.filesize # size of video in bytes
+    for _ in range(3):
+        try:
+            yt   = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+            yd = yt.streams.get_highest_resolution()
+            video_title = yt.title   # title of video
+            video_views = yt.views   # views in video
+            video_length = yt.length # length of video in seconds
+            video_size = yd.filesize # size of video in bytes
 
-        print("\nTitle: ", video_title)
-        print("Views: ", video_views)
-        print("Length: ", video_length//60, "minutes", video_length%60, "seconds")
-        print("Size: ", video_size/(2**20), "MB")
+            print("\nTitle: ", video_title)
+            print("Views: ", video_views)
+            print("Length: ", video_length//60, "minutes", video_length%60, "seconds")
+            print("Size: ", video_size/(2**20), "MB")
 
-        time.sleep(1)
-        print("Donwloading... ")
+            time.sleep(1)
+            print("Donwloading... ")
 
-        # Add folder path here
-        yd.download(".\YT")
+            # Add folder path here
+            yd.download(".\YT")
+            
+            print("Download complete!")
+            print("\n------------------------------------")
+
+            links.remove(link)
+            break
         
-        print("Download complete!")
-        print("\n------------------------------------")
-
-        links.remove(link)
-        break
-    
-        # except Exception:
-        #     print(f"\nAn error occurred while accessing the data for {link}")
-        #     continue
+        except Exception:
+            print(f"\nAn error occurred while accessing the data for {link}")
+            continue
 
 open('links.txt', 'w').close()
 
